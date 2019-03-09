04d6e15433a6e6669d08fc2a1d0b620c0be2653d
Andrew <andrewryanogle@gmail.com>
2019-03-09 11:27:56 -0800

    finished mvp added routes for projects and actions

--- routers/actionsRouter.js
+++ routers/actionsRouter.js
@@ -3,12 +3,80 @@ const actionModel = require("../data/helpers/actionModel");
 const router = express.Router();
 
 router.get("/actions", async (req, res) => {
-    try {
-      const action = await actionModel.get();
+  try {
+    const action = await actionModel.get();
+    res.status(200).json(action);
+  } catch (error) {
+    res.status(500).json({ error: "Could not retrive action information." });
+  }
+});
+router.get("/actions/:id", async (req, res) => {
+  try {
+    const action = await actionModel.get(req.params.id);
+    if (action) {
       res.status(200).json(action);
+    } else {
+      res.status(404).json({ message: "The specified action does not exist." });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Action information could not be retrieved." });
+  }
+});
+router.post("/actions", async (req, res) => {
+  try {
+    const action = await actionModel.insert(req.body);
+    if (action) {
+      res.status(201).json(action);
+    } else {
+      res.status(400).json({
+        errorMessage: "Please provide title and contents for the action."
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: "There was an error while saving the action to the database"
+    });
+  }
+});
+router.put("/actions/:id", async (req, res) => {
+    try {
+      const { description, notes } = req.body;
+      if (description && notes) {
+        const action = await actionModel.update(req.params.id, req.body);
+        if (action) {
+          res.status(200).json(action);
+        } else {
+          res
+            .status(404)
+            .json({ message: "The action with the specified ID does not exist." });
+        }
+      } else {
+        res.status(400).json({
+          errorMessage: "Please provide title and contents for the action."
+        });
+      }
     } catch (error) {
-      res.status(500).json({ error: "Could not retrive action information." });
+      res
+        .status(500)
+        .json({ error: "The action information could not be modified." });
     }
   });
+  router.delete("/actions/:id", async (req, res) => {
+    try {
+      const action = await actionModel.remove(req.params.id);
+      if (action) {
+        res.json(action);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The action with the specified ID does not exist." });
+      }
+    } catch (error) {
+      res.status(500).json({ error: "The action could not be removed" });
+    }
+  });
+  
 
 module.exports = router;
--- routers/projectsRouter.js
+++ routers/projectsRouter.js
@@ -1,5 +1,97 @@
 const express = require("express");
-const postDB = require("../data/helpers/projectModel");
+const projectModel = require("../data/helpers/projectModel");
 const router = express.Router();
+
+router.get("/projects", async (req, res) => {
+  try {
+    const project = await projectModel.get();
+    res.status(200).json(project);
+  } catch (error) {
+    res.status(500).json({ error: "Could not retrive action information." });
+  }
+});
+router.get("/projects/:id", async (req, res) => {
+  try {
+    const project = await projectModel.get(req.params.id);
+    if (action) {
+      res.status(200).json(project);
+    } else {
+      res.status(404).json({ message: "The specified project does not exist." });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Project information could not be retrieved." });
+  }
+});
+router.get("/projects/:id/actions", async (req, res) => {
+    try {
+      const project = await projectModel.getProjectActions(req.params.id);
+      if (action) {
+        res.status(200).json(project);
+      } else {
+        res.status(404).json({ message: "The specified project does not exist." });
+      }
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "Project actions could not be retrieved." });
+    }
+  });
+router.post("/projects", async (req, res) => {
+  try {
+    const project = await projectModel.insert(req.body);
+    if (project) {
+      res.status(201).json(project);
+    } else {
+      res.status(400).json({
+        errorMessage: "Please provide title and contents for the project."
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: "There was an error while saving the project to the database"
+    });
+  }
+});
+router.put("/projects/:id", async (req, res) => {
+  try {
+    const { description, notes } = req.body;
+    if (description && notes) {
+      const project = await projectModel.update(req.params.id, req.body);
+      if (action) {
+        res.status(200).json(project);
+      } else {
+        res
+          .status(404)
+          .json({
+            message: "The project with the specified ID does not exist."
+          });
+      }
+    } else {
+      res.status(400).json({
+        errorMessage: "Please provide title and contents for the project."
+      });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "The action information could not be modified." });
+  }
+});
+router.delete("/projects/:id", async (req, res) => {
+  try {
+    const project = await projectModel.remove(req.params.id);
+    if (project) {
+      res.json(project);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The project with the specified ID does not exist." });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "The project could not be removed" });
+  }
+});
 
 module.exports = router;
